3aa435210d99b552e78e2dd824d63c60e2f415e1
Daniel Berger <Daniel.Berger@L-P-A.com>
2020-04-19 23:58:30 +0200

    add unit test project

---
+++ HyperSimplices.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HyperSimplices.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HyperSimplices.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d284a0b1-a7f7-4909-86ab-dd60f42a2cba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HyperSimplices.UnitTests/VolumeTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HyperSimplices.UnitTests
+{
+    [TestClass]
+    public class VolumeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
